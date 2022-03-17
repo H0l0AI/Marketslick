9a17c4355c80ad70c858b0985e351d3d88c6a7b7
Alex Cashmore <alex@aider.ai>
2022-03-18 12:11:25 +1300

    fix main

--- src/pages/SecondaryPage.js
+++ src/pages/SecondaryPage.js
@@ -29,8 +29,10 @@ import {
     contactCTA,
     secondaryPhoto2, secondaryPhoto1, contactBlurb, p3Content1, p3Heading1, backgroundType,bgClass,imageURLArray,tLogo,hasScroll,font
 } from "../content";
-import {NavBar,scrollActivate} from "./HeroPage";
+import {scrollActivate} from "./HeroPage";
 import {rootStore} from "../stores/Store";
+import {NavBar} from "./MarketingHeroPage";
+
 
 
 export class SecondaryPage extends React.Component {
