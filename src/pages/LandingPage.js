85f788c29c8843a5f7598691ffa7d48fd6602058
Alex Cashmore <alex@aider.ai>
2023-09-16 17:17:11 +1200

    update var

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,7 +7,7 @@ import { BsCartCheck } from "react-icons/bs";
 
 
 const LandingPage = () => {
-    console.log('netlify env',process.env.NETLIFY_VAR)
+    console.log('netlify env',process.env.REACT_APP_NETLIFY_VAR)
   const isMobile = window.innerWidth <= 768;
   return (
     <div
